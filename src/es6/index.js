991d798a4ef7e78d26a4ff35971b13a758ebfd34
BryanValentierra <102090732+BryanValentierra@users.noreply.github.com>
2022-05-20 15:47:54 -0500

    Se vieron las diferencias entre let-const y var

--- src/es6/index.js
+++ src/es6/index.js
@@ -22,3 +22,51 @@ let epicPhrase2 = `${hello} ${world}`
 console.log(epicPhrase2);
 
 // Los templates literals sirven para concatenar variables y su sintaxis es ${}
+
+let lorem = "Qui consequatur. Commodi. Ipsum velduis yet minima \n" +   "otra frase epica que necesitamos.";
+
+// es6
+let lorem2 = `otra frase epica que necesitamos
+ahora es otra frase epica`;
+
+console.log(lorem);
+console.log(lorem2);
+
+let person = {
+    'name': 'oscar',
+    'age': 32,
+    'country': 'MX',
+}
+
+console.log(person.name, person.age);
+
+let { name, age, country} = person;
+
+console.log(name, age, country);
+
+
+let team1 = ['Oscar', 'Julian', 'Ricardo'];
+let team2 = ['Valeria', 'Yesica', 'Camila'];
+
+let education = ['David', ...team1, ...team2]; // Al usar los tres puntos, lo que estamos haciendo es crear una nueva lista con el valor ingresado y con los valores de las listas pasadas
+
+console.log(education);
+
+{
+    var globalVar = "Global Var";
+}
+
+{
+    let globalLet = 'Global Let';
+    console.log(globalLet);
+}
+
+console.log(globalVar);
+
+// Es mas recomendale en un bloque, usar las variables let y const, ya que, no se pueden redefinir por otro valor. En cambio el var si puede ser redefinido por otro valor, haciendo que reemplace el anterior y pueda ocasionar un error
+
+const a = 'b';
+a = 'a';
+
+
+
