55d73fa824bc41808b1d4739b06557a26568d19c
BryanValentierra <102090732+BryanValentierra@users.noreply.github.com>
2022-05-20 16:52:26 -0500

    Se aprendieron muchas cosas como los generators, el miport-export entre otras cosas

--- src/es6/index.js
+++ src/es6/index.js
@@ -38,11 +38,11 @@ let person = {
     'country': 'MX',
 }
 
-console.log(person.name, person.age);
+// console.log(person.name, person.age);
 
-let { name, age, country} = person;
+// let { name, age, country} = person;
 
-console.log(name, age, country);
+// console.log(name, age, country);
 
 
 let team1 = ['Oscar', 'Julian', 'Ricardo'];
@@ -68,5 +68,97 @@ console.log(globalVar);
 const a = 'b';
 a = 'a';
 
+let name = 'Bryan';
+let age = 18;
+
+// es5
+Obj = { name: name, age: age};
+
+// es6
+obj2 = { name, age};
+
+console.log(obj2);
+
+const names = [
+    {name: 'Sofia',
+      age: 17,},
+
+      {name: 'Bryan',
+      age: 18,}
+]
+
+let lisOfNames = names.map( function (item) {
+    console.log(item.name);
+})
+
+let lisOfNames2 = names.map(item => console.log(item.name));
+
+// Esta es otra forma de hacerlo con una funcion flecha y una funcion anonima
+const lisOfNames3 = (name, age, country) => {
+    // ... Aca el bloque de codigo
+}
+
+// Si solo se pasara un dato se puede ver también asi
+
+const lisOfNames4 = name => {
+    // ... y aca el bloque de codigo
+}
+
+// Esta forma es mucho mas amigable con arrow function
+const square = num => num* num;
 
 
+// Acá trabajaré con las promseas, la cual su función sera que ejecute una acción en un futuro
+const helloPromise = () => {
+    return new Promise((resolve, reject) =>{
+        if(false) {
+            resolve('Hey!');
+        } else {
+            reject('Ups!!');
+        }
+    });
+}
+
+helloPromise()
+.then(response =>console.log(response))
+.then(() => console.log('Hola'))
+.catch(error => console.log(error));
+
+
+// Acá veremos como crear una calculadora mediante una clase usandola y en ella creando un metodo
+class calculator {
+    constructor() { // La palabra constructor es escencial para crear una clase
+        this.valueA = 0;
+        this.valueB = 0;
+    }
+    // Este es el metodo
+    sum(valueA, valueB) {
+        this.valueA = valueA;
+        this.valueB = valueB;
+        return this.valueA  +   this.valueB;
+    }
+}
+
+const calc = new calculator ();
+
+console.log(calc.sum(2,2));
+
+//Acá estamos probando el import y export de otr archivo js
+import { hello } from './module';
+
+hello();
+
+// los generators son una función especial que retorna una serie de valores segun el algoritmo definido
+function* helloWorld () { // el * es clave para decirle a la terminal que este es un generator
+    if (true) {
+        yield 'Hello,';
+    }
+    if (true) {
+        yield 'World';
+    }
+}
+
+const generatorHello = helloWorld();
+console.log(generatorHello.next().value);
+console.log(generatorHello.next().value);
+console.log(generatorHello.next().value);
